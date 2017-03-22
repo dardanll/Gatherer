d6346cfcad691b98d83b03d65c3603650494598f
Dardan <dardanlladrovci@gmail.com>
2017-03-22 10:53:40 +0100

    Add CreatesProject class (action that creates project)

--- spec/features/add_project_spec.rb
+++ spec/features/add_project_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe "adding projects" do
 	it "allows a user to create a project with tasks" do
-		pending "Yet"
 		visit new_project_path
 		fill_in "Name", with: "Project Runway"
 		fill_in "Tasks", with: "Task 1:3\nTask 2:5"
